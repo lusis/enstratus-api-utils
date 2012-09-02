3c7686f1039ce1e0dd04322e13ad40affe413915
John E. Vincent <lusis.org+github.com@gmail.com>
2012-09-02 00:23:08 -0400

    a little formatting cleanup

--- ruby/signing.rb
+++ ruby/signing.rb
@@ -4,6 +4,7 @@ require 'json'
 require 'base64'
 require 'pp'
 
+# normally I'd do anything other than access_key and secret_key as an options hash
 def sign_request(access_key, secret_key, ua='enstratus.rb', http_method='GET', path='/geography/Cloud', api_version='2012-02-29')
 
   timestamp = (Time.now.to_i * 1000)
@@ -25,11 +26,17 @@ api_version = '2012-02-29'
 path = '/geography/Cloud'
 http_method='GET'
 timestamp = (Time.now.to_i * 1000)
+
 signature = sign_request(access_key, secret_key, ua=ua, http_method=http_method, path=path, api_version=api_version)
 
 connection = Excon.new('https://api.enstratus.com')
 
-headers = {'User-agent' => ua,'x-esauth-access' => access_key, 'x-esauth-timestamp' => timestamp, 'x-esauth-signature' => signature, 'x-es-details' => 'basic', 'Accept' => 'application/json'}
+headers = {'User-agent' => ua,
+           'x-esauth-access' => access_key,
+           'x-esauth-timestamp' => timestamp,
+           'x-esauth-signature' => signature,
+           'x-es-details' => 'basic',
+           'Accept' => 'application/json'}
 
 results = connection.get(:path => "/api/enstratus/#{api_version}/geography/Cloud", :headers => headers)
 
