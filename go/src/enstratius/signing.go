b8c960235107383f57210fc95e008d720a8a1545
John E. Vincent <lusis.org+github.com@gmail.com>
2013-08-07 14:11:17 -0400

    some structs for yo head

--- go/src/enstratius/signing.go
+++ go/src/enstratius/signing.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"net/http"
-	"io/ioutil"
-	"crypto/sha256"
 	"crypto/hmac"
+	"crypto/sha256"
 	"encoding/base64"
-	"strconv"
+	"encoding/json"
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 )
@@ -19,10 +21,41 @@ const ES_HOST = "https://api.enstratus.com"
 const ES_BASE_PATH = "/api/enstratus"
 const ES_API_VER = "2012-06-15"
 
+type Cloud struct {
+	Name                      string
+	ComputeDelegate           string
+	WebUrl                    string
+	CloudId                   int
+	Status                    string
+	ComputeEndpoint           string
+	PrivateCloud              bool
+	LogoUrl                   string
+	ComputeSecretKeyLabel     string
+	Provider                  string
+	Endpoint                  string
+	ComputeAccountNumberLabel string
+	DocumentationLabel        string
+	CloudProviderName         string
+	DaseinComputeDelegate     string
+	CloudProviderConsoleUrl   string
+	CloudProviderLogoUrl      string
+}
+
+type CloudList struct {
+	Clouds []Cloud
+}
+
+type EnstratusError struct {
+	// { "error" : { "message" : "Invalid access keys" } }
+	Error struct {
+		Message string
+	}
+}
+
 func GetTimeString() string {
-    s := time.Now()
-    now := s.Unix() * 1000 + int64(s.Nanosecond() / 1e6)
-    return strconv.FormatInt(now, 10)
+	s := time.Now()
+	now := s.Unix()*1000 + int64(s.Nanosecond()/1e6)
+	return strconv.FormatInt(now, 10)
 }
 
 func HmacB64(message string, secret string) string {
@@ -32,8 +65,8 @@ func HmacB64(message string, secret string) string {
 }
 
 func SignRequest(accessKey string, secretKey string,
-		userAgent string, httpMethod string,
-		httpPath string) string {
+	userAgent string, httpMethod string,
+	httpPath string) string {
 	signpath := strings.Join([]string{ES_BASE_PATH, ES_API_VER, httpPath}, "/")
 	timestamp := GetTimeString()
 	s := strings.Join([]string{accessKey, httpMethod, signpath, timestamp, userAgent}, ":")
@@ -60,8 +93,8 @@ func main() {
 	request.Header.Add("x-es-details", "basic")
 	request.Header.Add("user-agent", ES_UA)
 	sig := SignRequest(es_access_key, es_secret_key,
-				ES_UA, "GET",
-				"geography/Cloud")
+		ES_UA, "GET",
+		"geography/Cloud")
 	request.Header.Add("x-esauth-signature", sig)
 	client := &http.Client{}
 	response, err := client.Do(request)
@@ -75,6 +108,23 @@ func main() {
 			fmt.Printf("%s", err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s\n", string(contents))
+		switch {
+		case response.StatusCode >= 400 && response.StatusCode <= 599:
+			e := &EnstratusError{}
+			err := json.Unmarshal([]byte(contents), &e)
+			if err != nil {
+				fmt.Printf("JSON Decoding error: %s\n", err)
+				os.Exit(1)
+			}
+			fmt.Printf("%s\n", e.Error.Message)
+			os.Exit(1)
+		}
+		clouds := &CloudList{}
+		jsonerr := json.Unmarshal([]byte(contents), &clouds)
+		if jsonerr != nil {
+			fmt.Printf("JSON Decoding error: %s\n", jsonerr)
+			os.Exit(1)
+		}
+		spew.Dump(clouds)
 	}
 }
